36eaf25706f277b2c34b8db0f31e616932799385
qjw <wu_jia_qi1111@163.com>
2023-06-12 22:13:09 +0800

    2023年6月12日

--- src/main/java/com/jiaqi/wu/swaggerDemo/controller/IndexController.java
+++ src/main/java/com/jiaqi/wu/swaggerDemo/controller/IndexController.java
@@ -1,13 +1,10 @@
 package com.jiaqi.wu.swaggerDemo.controller;
 
 import com.jiaqi.wu.swaggerDemo.common.Result;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
+import com.jiaqi.wu.swaggerDemo.pojo.PsctechVo;
+import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author WJQ
@@ -21,6 +18,31 @@ public class IndexController {
     @GetMapping(value = "index", produces = "application/json")
     @ApiOperation(value = "index首页访问", notes = "index the notes", httpMethod = "GET", response = Result.class)
     public Result<Void> index() {
+        return Result.success();
+    }
+
+
+    @ApiOperation(value = "从jcforms获取代办列表", httpMethod = "GET")
+    @GetMapping("front_getWorkList")
+    public Result<Void> getFrontList(@RequestParam(required = false) @ApiParam(name = "pageSize", value = "每页显示的数量，为空时默认显示10条") Integer pageSize,
+                                     @RequestParam(required = false) @ApiParam(name = "pageNumber", value = "显示的页数，为空时默认显示第1页") Integer pageNumber,
+                                     @RequestParam(required = false) @ApiParam(name = "sysname", value = "系统名称，这里的系统名称只待办事项的名称，即：processChName") String sysname,
+                                     @RequestParam(required = false) @ApiParam(name = "type", value = "默认等于0,") Integer type,
+                                     @RequestParam(required = false) @ApiParam(name = "state", value = "默认等于-1") Integer state,
+                                     @RequestParam(required = false) @ApiParam(name = "sysid", value = "系统id，从front_getWorkCount接口返回的列表中取值") String sysid,
+                                     @RequestParam(required = false) @ApiParam(name = "sqr", value = "搜索标题关键字") String sqr,
+                                     @RequestParam(required = false) @ApiParam(name = "starttime", value = "开始时间") String starttime,
+                                     @RequestParam(required = false) @ApiParam(name = "endtime", value = "结束时间") String endtime,
+                                     @RequestParam(required = false) @ApiParam(name = "title", value = "搜索标题关键字") String title) {
+        log.info("getFrontList请求参数:pageSize:{}, pageNumber:{}, sysname:{}, type:{}", pageSize, pageNumber, sysname, type);
+        return Result.success();
+    }
+
+
+    @ApiOperation(value = "从jcforms获取代办列表Copy", httpMethod = "POST")
+    @PostMapping("front_getWorkList2")
+    public Result<Void> getFrontListCopy(@RequestBody PsctechVo psctechVo) {
+        log.info("getFrontListCopy请求参数:PsctechVo:{}", psctechVo);
         return Result.success();
     }
 
---
+++ src/main/java/com/jiaqi/wu/swaggerDemo/pojo/PsctechVo.java
@@ -0,0 +1,19 @@
+package com.jiaqi.wu.swaggerDemo.pojo;
+
+import lombok.Data;
+
+/**
+ * @author wjq
+ */
+@Data
+public class PsctechVo {
+
+    private Integer pageSize;
+    private Integer pageNumber;
+    private String sysname;
+    private String sysid;
+    private String starttime;
+    private String endtime;
+
+
+}
